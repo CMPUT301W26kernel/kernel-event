973f4aa7ad0ae42b636f2262ce82e62ad1496e6e
LagzaniaCat <grace.l.mackenzie@icloud.com>
2026-02-28 14:43:40 -0700

    Added Set Up and Waiting List fragments.

--- app_code/app/src/main/java/com/example/eventlottery/SetUpFragment.java
+++ app_code/app/src/main/java/com/example/eventlottery/SetUpFragment.java
@@ -1,7 +1,18 @@
+/**
+ * Set Up Fragment
+ * Sets up a new user with a profile.
+ * Last Modified: 2026-02-28 by Grace MacKenzie
+ *
+ * @author author1
+ * @author author2
+ * @since 2026-02-28
+ */
 package com.example.eventlottery;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,49 +21,16 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SetUpFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class SetUpFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SetUpFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SetUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SetUpFragment newInstance(String param1, String param2) {
-        SetUpFragment fragment = new SetUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +38,12 @@ public class SetUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_set_up, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        /*
+            TODO: Complete fragment logic
+         */
     }
 }
--- app_code/app/src/main/java/com/example/eventlottery/WaitingListDialogFragment.java
+++ app_code/app/src/main/java/com/example/eventlottery/WaitingListDialogFragment.java
@@ -1,64 +1,79 @@
+/**
+ * Waiting List Dialog Fragment
+ * Displays the users in a the waiting list of an event
+ * Last Modified: 2026-02-28 by Grace MacKenzie
+ *
+ * @author author1
+ * @author author2
+ * @since 2026-02-28
+ */
 package com.example.eventlottery;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link WaitingListDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class WaitingListDialogFragment extends Fragment {
+public class WaitingListDialogFragment extends DialogFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WaitingListDialogFragment() {
-        // Required empty public constructor
+    public interface WaitingListDialogListener {
+        void editEmoticon(String newEmoticon);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WaitingListDialogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WaitingListDialogFragment newInstance(String param1, String param2) {
-        WaitingListDialogFragment fragment = new WaitingListDialogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private WaitingListDialogListener listener;
+
+    // FUNCTIONS
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        Fragment parent = getParentFragment();
+        if (parent instanceof WaitingListDialogListener) {
+            listener = (WaitingListDialogListener) parent;
+        } else {
+            throw new RuntimeException(parent + " must implement WaitingListDialogListener");
         }
     }
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_waiting_list_dialog, container, false);
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // get the emoticon data if we passed any to it
+        Bundle args = getArguments();
+
+        View view = getLayoutInflater().inflate(R.layout.fragment_waiting_list_dialog, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        return builder
+                .setView(view)
+                .setTitle("Add/edit a city")
+                .setNegativeButton("Cancel", null)
+                .create();
+    }
+
+    /**
+     * Creates a new instance of EditEmoticonFragment and passes an emoticon to it.
+     * @param arg1 the selected emoticon before editing
+     * @return a new WaitingListDialogFragment with the selected emoticon data passed to it
+     */
+    static WaitingListDialogFragment newInstance(String arg1) {
+        Bundle args = new Bundle();
+        args.putString("emoticon", arg1);
+
+        WaitingListDialogFragment fragment = new WaitingListDialogFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 }
