79f8a18b14df93edc1385a1ea5055874f19ca82a
LagzaniaCat <grace.l.mackenzie@icloud.com>
2026-03-22 10:48:45 -0600

    Removed line adding root HomePageFragment to backstack.

--- code/app/src/main/java/com/example/eventlottery/MainActivity.java
+++ code/app/src/main/java/com/example/eventlottery/MainActivity.java
@@ -61,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, new HomePageFragment())
-                .addToBackStack(null)
                 .commit();
     }
 
